954f08efde75dfda65e531abbff899a40895242d
Limes Braum <limesbra@ob-h8.nsk.21-school.ru>
2023-07-27 10:24:35 +0700

    first commit

---
+++ src/SmartCalc_V1.h
@@ -0,0 +1,13 @@
+#ifndef SmartCalc_v1_H
+#define SmartCalc_v1_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 1
+#define FAILURE 0
+#define EPS 0.0000001
+
+
+#endif
