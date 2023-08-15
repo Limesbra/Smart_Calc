bba4c4a18b61add82a327d536bda041b4c97fab5
Limes Braum <limesbra@ob-h8.nsk.21-school.ru>
2023-08-15 17:55:09 +0700

    develop: added func of  Reverse Polish notation

--- src/experimental.c
+++ src/experimental.c
@@ -32,10 +32,11 @@ typedef enum{
     plus_or_minus,
     mult_or_div,
     mod = 2,
-    pow_or_sqrt,
-    log_or_ln,
-    trigonometry,
-    scobe
+    PPOW,
+    PSQRT = -2,
+    log_or_ln = -3,
+    trigonometry = -4,
+    scobe = 6
 }lexeme_priority;
 
 // typedef struct {
@@ -53,7 +54,8 @@ typedef struct {
     int counter;
 }L;
 
-int parser(char *expression, char *expression_for_x);
+int S21_SmartCalc(char *expression, char *expression_for_x, char *answer);
+int parser(char *expression, char *expression_for_x, L *pars);
 void setInputLexeme(L *pars, double num, lexeme_type type, lexeme_priority type_1, int *step);
 int trigonometryCheck(char *expression, L *pars, int error);
 int scobeChecker(char *expression, L *pars, int *s_counter, int *error);
@@ -61,12 +63,16 @@ int logChecker(char *expression, L *pars);
 int arithmeticSign(char *expression, L *pars, int error);
 int validSign(char *expression, L *pars);
 int constAnalyze(char *expression, char *expression_for_x, L *pars, int error);
-int n (char *expression, L *pars);
+int handlerNum (char *expression, L *pars);
+void RPN(L *pars, L *output, L *stack);
 
 int main(){
     char expression[255] = "23+44*(cos(11))";
     char expression_for_x[255] = "\0";
-    parser(expression, expression_for_x);
+    char answer[255] = "";
+    // L pars ={0, 0, 0, 0};
+    S21_SmartCalc(expression, expression_for_x, answer);
+    // parser(expression, expression_for_x, &pars);
 
     // L pars ={0};
     // char expression[255] = "()";
@@ -77,25 +83,42 @@ int main(){
     // printf("%d\n", x);
     // printf("%d\n", error);
 
-   
 
     return 0;
 }
 
-int parser(char *expression, char *expression_for_x){
+int S21_SmartCalc(char *expression, char *expression_for_x, char *answer){
+    L pars ={0, 0, 0, 0};
+    L output = {0, 0, 0, 0};
+    L stack = {0, 0, 0, 0};
+    int error = 0;
+    error = parser(expression, expression_for_x, &pars);
+    if(error == 0){
+        RPN(&pars, &output, &stack);
+        // mathProcess();
+        for(int i = 0; i != output.counter; i++){
+            printf("%f ", output.value[i]);
+            printf("%d ", output.priority[i]);
+            printf("%d ", output.type[i]);
+            printf("\n");
+    }
+    }
+    return error;
+}
+
+int parser(char *expression, char *expression_for_x, L *pars){
     int error = 0;
     int i = 0;
-    L pars ={0, 0, 0, 0};
     int s_counter = 0;
     while(*expression != '\0' && error == 0){
 
         // expression += 2;
-        expression += n (expression, &pars);
-        expression += trigonometryCheck(expression, &pars, error);
-        expression += scobeChecker(expression, &pars, &s_counter, &error);
-        expression += logChecker(expression, &pars);
-        expression += arithmeticSign(expression, &pars, error);
-        expression += constAnalyze(expression, expression_for_x, &pars, error);
+        expression += handlerNum (expression, pars);
+        expression += trigonometryCheck(expression, pars, error);
+        expression += scobeChecker(expression, pars, &s_counter, &error);
+        expression += logChecker(expression, pars);
+        expression += arithmeticSign(expression, pars, error);
+        expression += constAnalyze(expression, expression_for_x, pars, error);
         
 
         // if(isdigit(*expression)){
@@ -106,13 +129,13 @@ int parser(char *expression, char *expression_for_x){
         if(*expression == '.'){error = 1;}
         // else {error = 1;}
     }
-    for(int i = 0; i != pars.counter; i++){
-    printf("%f ", pars.value[i]);
-    printf("%d ", pars.priority[i]);
-    printf("%d ", pars.type[i]);
-    printf("%d", pars.counter);
-    printf("\n");
-    }
+    // for(int i = 0; i != pars -> counter; i++){
+    // printf("%f ", pars -> value[i]);
+    // printf("%d ", pars -> priority[i]);
+    // printf("%d ", pars -> type[i]);
+    // printf("%d", pars -> counter);
+    // printf("\n");
+    // }
     return error;
 }
 
@@ -156,14 +179,15 @@ int trigonometryCheck(char *expression, L *pars, int error){
 
 int scobeChecker(char *expression, L *pars, int *s_counter, int *error){
     int step = 0;
-    if(*expression != '\0'){
-        if(*expression == '('){
+    if(*expression != '\0' && (*expression == '(' ||*expression == ')')){
+        if(*expression == '(' && *(expression +1 ) != ')'){
             setInputLexeme(pars, 0, leftScobe, scobe, &step);
             *s_counter += 1;
         }else if(*expression == ')' && s_counter != 0){
             setInputLexeme(pars, 0, rightScobe, scobe, &step);
             *s_counter -= 1;
-        }/*else{*error = 1;}*/
+        }
+        else{*error = 1;}
     }
     
     return step;
@@ -197,9 +221,9 @@ int arithmeticSign(char *expression, L *pars, int error){
     }else if(*expression == '%' && valid){
         setInputLexeme(pars, 0, MOD, mod, &step);
     }else if(*expression == '^' && valid){
-        setInputLexeme(pars, 0, POW, pow_or_sqrt, &step);}
+        setInputLexeme(pars, 0, POW, PPOW, &step);}
     else if(*expression == 's' && *(expression + 1) == 'q' && *(expression + 2) == 'r' && *(expression + 2) == 't' && valid){
-        setInputLexeme(pars, 0, SQRT, pow_or_sqrt, &step);
+        setInputLexeme(pars, 0, SQRT, PSQRT, &step);
     }else{
         error = 1;}
     return step;
@@ -234,16 +258,82 @@ int constAnalyze(char *expression, char *expression_for_x, L *pars, int error){
     return step;
 }
 
-int n (char *expression, L *pars){
-    int i = 0;
+int handlerNum (char *expression, L *pars){
+    int step = 0;
     if(isdigit(*expression)){
-        while(isdigit(*(expression + i))){
-            i++;
+        while(isdigit(*(expression + step))){
+            step++;
         }
         double num = strtod(expression, &expression);
         int step = 0;
         setInputLexeme(pars, num, NUM, number, &step);
     }
 
-    return i;
+    return step;
+}
+
+void RPN(L *pars, L *output, L *stack){
+    for(int i = 0; i < pars ->counter; i ++){
+        if(pars ->type[i] == 0){
+            output->value[0 + output->counter] = pars ->value[i];
+            output->type[0 + output->counter] = pars ->type[i];
+            output->priority[0 + output->counter] = pars ->priority[i];
+            output->counter += 1;
+        }
+        else if(pars ->type[i] == 16/*|| pars ->priority[i] == -2 || pars ->priority[i] == -3 || pars ->priority[i] == -4*/){
+            stack->value[0 + stack->counter] = pars ->value[i];
+            stack->type[0 + stack->counter] = pars ->type[i];
+            stack->priority[0 + stack->counter] = pars ->priority[i];
+            stack->counter += 1; 
+        }
+        else if(pars ->priority[i] == -2 || pars ->priority[i] == -3 || pars ->priority[i] == -4){
+            output->value[0 + output->counter] = pars ->value[i];
+            output->type[0 + output->counter] = pars ->type[i];
+            output->priority[0 + output->counter] = pars ->priority[i];
+            output->counter += 1; 
+        }
+        else if(pars ->priority[i] == 1 || pars ->priority[i] == 2){
+            if(stack->counter == 0){
+                stack->value[0 + stack->counter] = pars ->value[i];
+                stack->type[0 + stack->counter] = pars ->type[i];
+                stack->priority[0 + stack->counter] = pars ->priority[i];
+                stack->counter += 1; 
+            }
+            else if(pars ->priority[i] >= stack->priority[stack->counter - 1]){
+                stack->value[0 + stack->counter] = pars ->value[i];
+                stack->type[0 + stack->counter] = pars ->type[i];
+                stack->priority[0 + stack->counter] = pars ->priority[i];
+                stack->counter += 1;
+            }
+            else{
+                stack->value[0 + stack->counter] = pars ->value[i];
+                stack->type[0 + stack->counter] = pars ->type[i];
+                stack->priority[0 + stack->counter] = pars ->priority[i];
+                stack->counter += 1; 
+            }
+        }
+        else if(pars ->type[i] == 17){
+            while (stack->type[stack->counter - 1] != 16){
+                output->value[0 + output->counter] = stack->value[stack->counter - 1];
+                output->type[0 + output->counter] = stack->type[stack->counter - 1];
+                output->priority[0 + output->counter] = stack->priority[stack->counter - 1];
+                output->counter += 1;
+                stack->counter -= 1;
+            }
+            if(stack->type[stack->counter - 1] == 16){
+                stack->value[stack->counter - 1] = 0;
+                stack->type[stack->counter - 1] = 0;
+                stack->priority[stack->counter - 1] = 0;
+                stack->counter -= 1;
+            }
+        }
+    }
+    while(stack->counter != 0){
+        output->value[0 + output->counter] = stack->value[stack->counter - 1];
+        output->type[0 + output->counter] = stack->type[stack->counter - 1];
+        output->priority[0 + output->counter] = stack->priority[stack->counter - 1];
+        output->counter += 1;
+        stack->counter -= 1;
+    }
+
 }
